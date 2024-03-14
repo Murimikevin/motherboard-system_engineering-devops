6f0988efd3766972f83ef474f417b16f44850240
Murimikevin <kamaukevin069@gmail.com>
2024-03-14 15:37:45 +0300

    filetxtojs.sh

---
+++ 0x03-shell_exercises/filetxtojs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+parent_dir=~/home/user/departments
+mkdir -p "$parent_dir/physics"
+mkdir -p "$parent_dir/hospitality"
+touch "$parent_dir/physics/file.txt"
+touch "$parent_dir/physics/file.o"
+touch "$parent_dir/physics/file.js"
+touch "$parent_dir/hospitality/file.txt"
+touch "$parent_dir/hospitality/file.o"
+touch "$parent_dir/hospitality/file.js"
